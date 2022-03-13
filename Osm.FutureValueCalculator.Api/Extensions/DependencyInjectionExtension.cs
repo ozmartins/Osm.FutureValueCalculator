61906dd7e6a7c5beb0bec03c8d884687d13bd1e1
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2022-03-13 10:28:05 -0300

    Initial project configuration

---
+++ Osm.FutureValueCalculator.Api/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Osm.FutureValueCalculator.Api.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {           
+            return services;
+        }
+    }
+}
